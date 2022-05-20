74501288901dd5966ac26c13292d40d0200a8525
DisasterNull <tomoya.17843@gmail.com>
2022-05-21 05:40:34 +0900

    技術的問題が発生しました。再度お試しください。を対策 count=100 までは対応

--- web.py
+++ web.py
@@ -79,7 +79,6 @@ for i in range(count):
                 yes.click()
 
                 print('はいを選択しました')
-                time.sleep(3)
                 break
 
         else:
@@ -92,7 +91,6 @@ for i in range(count):
                     probably.click()
 
                     print('多分そう部分的にそうを選択しました')
-                    time.sleep(3)
                     break
 
             else:
@@ -100,7 +98,6 @@ for i in range(count):
                 no.click()
 
                 print('いいえを選択しました')
-                time.sleep(3)
 
         proposal_title = browser.find_elements_by_class_name('proposal-title')
 
@@ -112,19 +109,18 @@ for i in range(count):
         propose_yes.click()
 
         print('はいを選択肢しました')
-        time.sleep(3)
-
+    elif proposal_title.text == '技術的問題が発生しました。再度お試しください。':
+        browser.refresh()
+        i -= 1
     else:
         propose_no = browser.find_element_by_id('a_propose_no')
         propose_no.click()
 
         print('いいえを選択肢しました')
-        time.sleep(3)
 
     # もう一度遊ぶ
     btn_again = browser.find_element_by_link_text('もう一度遊ぶ')
     btn_again.click()
 
     print('もう一度遊ぶを選択しました')
-    time.sleep(3)
 browser.close()
