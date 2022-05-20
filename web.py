9f2b4e44f91af066d2795fe435c84967163e2ad9
DisasterNull <tomoya.17843@gmail.com>
2022-05-20 15:03:15 +0900

    変数名の変更

--- web.py
+++ web.py
@@ -22,14 +22,14 @@ browser.get(url_login)
 time.sleep(3)
 
 # 開始する
-browser_from = browser.find_element_by_class_name('btn-play')
-browser_from.click()
+btn_play = browser.find_element_by_class_name('btn-play')
+btn_play.click()
 print('開始しました')
 time.sleep(3)
 
 # ゲームモード選択
-browser_from = browser.find_element_by_link_text('キャラクター')
-browser_from.click()
+btn_character = browser.find_element_by_link_text('キャラクター')
+btn_character.click()
 print('ゲームモードを選択しました')
 time.sleep(3)
 
@@ -50,58 +50,77 @@ datasets_yes = ['年齢は20代ですか？', '25歳よりも若い?',
 
 # 「多分そう部分的にそう」を選ぶべき質問
 datasets_maybe = [
-    "音楽に関係してる?", "歌に関係ありますか？",
+    "音楽に関係してる?", "歌に関係ありますか？", '様々な種類のクローンがいますか？'
 ]
 
+# 処理開始
 for i in range(count):
     print('\n\n' + str(i+1) + "回目")
-    flag_answer = browser.find_elements_by_class_name('proposal-title')
-    while not flag_answer:
+
+    proposal_title = browser.find_elements_by_class_name('proposal-title')
+
+    while not proposal_title:
         question_text = browser.find_element_by_class_name('question-text').text
+
         while question_text == '取り込み中・・・':
             question_text = browser.find_element_by_class_name(
                 'question-text').text
+
         print(question_text)
+
         for yes in datasets_yes:
             flag_yes = question_text == yes
+
             if flag_yes:
-                click_btn = browser.find_element_by_id('a_yes')
-                click_btn.click()
+                yes = browser.find_element_by_id('a_yes')
+                yes.click()
+
                 print('はいを選択しました')
                 time.sleep(3)
                 break
+
         else:
 
             for maybe in datasets_maybe:
                 flag_maybe = question_text == maybe
+
                 if flag_maybe:
-                    click_btn = browser.find_element_by_id('a_probably')
-                    click_btn.click()
+                    probably = browser.find_element_by_id('a_probably')
+                    probably.click()
+
                     print('多分そう部分的にそうを選択しました')
                     time.sleep(3)
                     break
+
             else:
-                click_btn = browser.find_element_by_id('a_no')
-                click_btn.click()
+                no = browser.find_element_by_id('a_no')
+                no.click()
+
                 print('いいえを選択しました')
                 time.sleep(3)
-        flag_answer = browser.find_elements_by_class_name('proposal-title')
-    flag_answer = browser.find_element_by_class_name('proposal-title')
-    print(flag_answer.text + 'を思い浮かべています')
 
-    if flag_answer.text == 'ぽゅん':
+        proposal_title = browser.find_elements_by_class_name('proposal-title')
+
+    proposal_title = browser.find_element_by_class_name('proposal-title')
+    print(proposal_title.text + 'を思い浮かべています')
+
+    if proposal_title.text == 'ぽゅん':
         propose_yes = browser.find_element_by_id('a_propose_yes')
         propose_yes.click()
+
         print('はいを選択肢しました')
         time.sleep(3)
+
     else:
         propose_no = browser.find_element_by_id('a_propose_no')
         propose_no.click()
+
         print('いいえを選択肢しました')
         time.sleep(3)
 
     # もう一度遊ぶ
-    browser_from = browser.find_element_by_link_text('もう一度遊ぶ')
-    browser_from.click()
+    btn_again = browser.find_element_by_link_text('もう一度遊ぶ')
+    btn_again.click()
+
     print('もう一度遊ぶを選択しました')
     time.sleep(3)
